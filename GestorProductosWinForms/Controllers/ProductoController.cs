f5d79c9c0cc3bb371c08f1567e05a0f7870890b3
agusposti <agusposti9@gmail.com>
2026-08-22 19:38:48 -0300

    primer subida proyecto

---
+++ GestorProductosWinForms/Controllers/ProductoController.cs
@@ -0,0 +1,31 @@
+﻿using GestorProductosWinForms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorProductosWinForms.Controllers
+{
+    public class ProductoController
+    {
+        private List<Producto> productos;
+
+        public ProductoController()
+        {
+            productos = new List<Producto>();
+        }
+
+
+        public void agregarProducto(string nombre, decimal precio, int stock)
+        {
+            Producto productos = new Producto(nombre, precio, stock);
+        }
+
+        public List<Producto> ObtenerProductos()
+        {
+            return productos;
+        }
+    }
+}
---
+++ GestorProductosWinForms/Models/Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorProductosWinForms.Models
+{
+    public class Producto : IEntidad
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+
+        public Producto(string nombre, decimal precio, int stock)
+        {
+            Nombre = nombre;
+            Precio = precio;
+            Stock = stock;
+        }
+    }
+}
